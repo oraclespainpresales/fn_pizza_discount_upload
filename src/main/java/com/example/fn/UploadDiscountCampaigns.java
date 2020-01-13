ed5c0edd24ca77cc9ea1f63d7a4fe48c7aabb19e
test_oke <ivan.sampedro@oracle.com>
2020-01-13 06:29:21 -0800

    fisrt commit

---
+++ src/main/java/com/example/fn/UploadDiscountCampaigns.java
@@ -0,0 +1,11 @@
+package com.example.fn;
+
+public class UploadDiscountCampaigns {
+
+    public String handleRequest(String input) {
+        String name = (input == null || input.isEmpty()) ? "world"  : input;
+
+        return "Hello, " + name + "!";
+    }
+
+}
